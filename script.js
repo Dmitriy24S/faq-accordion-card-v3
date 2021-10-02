26f3ea76293138a23a1fef05041a70cc21470134
Dmitriy <devilins24@gmail.com>
2021-10-02 17:00:36 +0300

    add desktop design, style work

--- script.js
+++ script.js
@@ -5,3 +5,35 @@ questions.forEach((question) => {
     question.classList.toggle("show__text");
   });
 });
+
+// Box animation
+const box = document.querySelector(".img__box-container");
+const questionCard = document.querySelectorAll(".questions");
+const moveBoxLeft = () => {
+  box.style.left = "-579px";
+};
+const moveBoxRight = () => {
+  box.style.left = "-543px";
+};
+// Box animation for dekstop view on page load
+if (window.innerWidth > 1200) {
+  questionCard.forEach((question) => {
+    question.addEventListener("mouseover", moveBoxLeft);
+    question.addEventListener("mouseleave", moveBoxRight);
+  });
+}
+// Check for window resize and disable box animation for mobile view
+window.addEventListener("resize", function () {
+  if (window.innerWidth > 1200) {
+    questionCard.forEach((question) => {
+      question.addEventListener("mouseover", moveBoxLeft);
+      question.addEventListener("mouseleave", moveBoxRight);
+    });
+  }
+  if (window.innerWidth < 1200) {
+    questionCard.forEach((question) => {
+      question.removeEventListener("mouseover", moveBoxLeft);
+      question.removeEventListener("mouseleave", moveBoxRight);
+    });
+  }
+});
